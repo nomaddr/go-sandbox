74b73dccac9c831c527d01e7abf48beaea6fb664
nomaddr <robin.daub1@gmail.com>
2024-05-30 10:23:09 +0200

    added some stuff

--- main.go
+++ main.go
@@ -24,54 +24,6 @@ type Enemy struct {
 	SegmentDist float32
 }
 
-func NewEnemy(numSegments int, speed float32, segmentDist float32) *Enemy {
-	segments := make([]Segment, numSegments)
-	for i := 0; i < numSegments; i++ {
-		segments[i] = Segment{Position: rl.Vector2{X: -segmentDist * float32(i), Y: 0}}
-	}
-	return &Enemy{
-		Head:        rl.Vector2{X: 400, Y: 225},
-		Segments:    segments,
-		Speed:       speed,
-		SegmentDist: segmentDist,
-	}
-}
-
-func (e *Enemy) Update(target rl.Vector2) {
-	// Move the head towards the target
-	if rl.Vector2Distance(e.Head, target) > e.Speed*rl.GetFrameTime() {
-		e.Head = rl.Vector2MoveTowards(e.Head, target, e.Speed*rl.GetFrameTime())
-	}
-
-	// Update each segment to follow the previous one
-	for i := range e.Segments {
-		if i == 0 {
-			// The first segment follows the head
-			if rl.Vector2Distance(e.Segments[i].Position, e.Head) > e.SegmentDist {
-				e.Segments[i].Position = rl.Vector2MoveTowards(e.Segments[i].Position, e.Head, e.Speed*rl.GetFrameTime())
-			}
-		} else {
-			// Each segment follows the previous segment
-			if rl.Vector2Distance(e.Segments[i].Position, e.Segments[i-1].Position) > e.SegmentDist {
-				e.Segments[i].Position = rl.Vector2MoveTowards(e.Segments[i].Position, e.Segments[i-1].Position, e.Speed*rl.GetFrameTime())
-			}
-		}
-	}
-}
-
-func (e *Enemy) Draw() {
-	rl.DrawCircleV(e.Head, 20, rl.Maroon)
-	for i := 0; i < len(e.Segments); i++ {
-		rl.DrawCircleV(e.Segments[i].Position, 14, rl.Maroon)
-		switch i {
-		case 0:
-			rl.DrawLineEx(e.Segments[i].Position, e.Head, 10, rl.Maroon)
-		default:
-			rl.DrawLineEx(e.Segments[i].Position, e.Segments[i-1].Position, 10, rl.Maroon)
-		}
-	}
-}
-
 type Player struct {
 	pos      rl.Vector2
 	dir      rl.Vector2
@@ -107,7 +59,61 @@ func main() {
 	rl.CloseWindow()
 }
 
-// Init Player
+// #############
+// Enemy Methods
+// #############
+func NewEnemy(numSegments int, speed float32, segmentDist float32) *Enemy {
+	segments := make([]Segment, numSegments)
+	for i := 0; i < numSegments; i++ {
+		segments[i] = Segment{Position: rl.Vector2{X: -segmentDist * float32(i), Y: 0}}
+	}
+	return &Enemy{
+		Head:        rl.Vector2{X: 400, Y: 225},
+		Segments:    segments,
+		Speed:       speed,
+		SegmentDist: segmentDist,
+	}
+}
+
+func (e *Enemy) Update(target rl.Vector2) {
+	// Move the head towards the target
+	if rl.Vector2Distance(e.Head, target) > e.Speed*rl.GetFrameTime() {
+		e.Head = rl.Vector2MoveTowards(e.Head, target, e.Speed*rl.GetFrameTime())
+	}
+
+	// Update each segment to follow the previous one
+	for i := range e.Segments {
+		if i == 0 {
+			// The first segment follows the head
+			if rl.Vector2Distance(e.Segments[i].Position, e.Head) > e.SegmentDist {
+				e.Segments[i].Position = rl.Vector2MoveTowards(e.Segments[i].Position, e.Head, e.Segments[i].Speed*rl.GetFrameTime())
+			}
+		} else {
+			// Each segment follows the previous segment
+			if rl.Vector2Distance(e.Segments[i].Position, e.Segments[i-1].Position) > e.SegmentDist {
+				e.Segments[i].Position = rl.Vector2MoveTowards(e.Segments[i].Position, e.Segments[i-1].Position, e.Segments[i].Speed*rl.GetFrameTime())
+			}
+		}
+	}
+}
+
+func (e *Enemy) Draw() {
+	rl.DrawCircleV(e.Head, 20, rl.Maroon)
+	for i := 0; i < len(e.Segments); i++ {
+		rl.DrawCircleV(e.Segments[i].Position, 14, rl.Maroon)
+		switch i {
+		case 0:
+			rl.DrawLineEx(e.Segments[i].Position, e.Head, 10, rl.Maroon)
+		default:
+			rl.DrawLineEx(e.Segments[i].Position, e.Segments[i-1].Position, 10, rl.Maroon)
+		}
+	}
+}
+
+// ##############
+// Player Methods
+// ##############
+
 func (p *Player) Init(length int) {
 	p.pos = rl.NewVector2(float32(screenWidth)/2, float32(screenHeight)/2)
 	p.isMoving = false
@@ -117,7 +123,14 @@ func (p *Player) Init(length int) {
 	p.segments = make([]rl.Vector2, p.length)
 }
 
+func (p *Player) Shoot() {
+
+}
+
+// ######################
 // Init - Initialize game
+// ######################
+
 func Init() {
 	player = Player{}
 	player.Init(5)
@@ -128,7 +141,11 @@ func Init() {
 
 }
 
-// Input - Game input
+// ####################
+//
+//	Input - Game input
+//
+// ####################
 func Input() {
 	// control
 	if rl.IsKeyDown(rl.KeyW) {
@@ -155,12 +172,20 @@ func Input() {
 
 }
 
-// Update - Update game
+// ######################
+//
+//	Update - Update game
+//
+// ######################
 func Update() {
 	enemy.Update(mousePos)
 }
 
-// Draw - Draw game
+// ##################
+//
+//	Draw - Draw game
+//
+// ##################
 func Draw() {
 	rl.BeginDrawing()
 	rl.ClearBackground(rl.DarkGray)
